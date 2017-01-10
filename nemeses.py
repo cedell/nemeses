2e352efe69409ba4dfebed59c24329e13f4057ee
cedell <chris.edell@gmail.com>
2017-01-10 11:54:21 -0500

    Update nemeses.py

--- nemeses.py
+++ nemeses.py
@@ -1,16 +1,24 @@
+#!/usr/bin/env python3
 import datetime
-import time
 import io
 import subprocess
+import time
 import re
+import sys
+from scapy.all import srp
+from scapy.all import Ether, ARP, conf, arping
+
 active_violators = {}
 active_strangers = {}
 blacklist_dict = {}
 whitelist_dict = {}
-network_prefix = '192'
+iprange = '192.168.1.0/24'
+scan_retries = 10
+scan_timeout = 3
 
 
 def update_configs():
+    """Initiates functions which gather host details from configuration files."""
     global blacklist_dict
     global whitelist_dict
     blacklist_dict = update_config_dict('blacklist.conf', blacklist_dict)
@@ -18,25 +26,27 @@ def update_configs():
 
 
 def update_config_dict(conf_file, global_list):
+    """Opens specified host file and returns values found."""
     with open(conf_file, 'r') as file:
         latest_dict = {}
         for line in file:
-            # Skip comments
+            # Skip lines with comment hash
             if line[:1] == "#":
                 continue
-            # Regex to collapse tabs, strip newline, and then split by tabs
+            # Regex to collapse tabs, strip newline, and then split by tabs.
             line_values = re.sub('\t\t+', '\t', line).rstrip('\n').split('\t')
             latest_dict[line_values[0]] = line_values[1]
 
-        # Update the actively used black/whitelist if file shows new values
+        # Update the actively used black/whitelist if file shows new values.
         if latest_dict != global_list:
             send_to_log('{} updated to: {}'.format(conf_file.split('.')[0].capitalize(), latest_dict))
 
         return latest_dict
 
 
-def find_violators():
-    violators_found = blacklist_scanner()
+def find_blacklisters():
+    """Initiates and determines if blacklisted hosts are active."""
+    violators_found = blacklist_scanner_scapy()
     scan_time = time.time()
 
     # Add any new online host to dictionary.
@@ -46,7 +56,6 @@ def find_violators():
             h_ip = str(violators_found[host]['ip'])
             active_violators[host] = {'name': h_name, 'ip': h_ip, 'start_time': scan_time}
 
-            # print('Found {} with an IP of {}.'.format(h_name, h_ip))
             send_to_log('{} ({}) is now online'.format(h_name, h_ip))
 
     # Now verify what hosts have gone offline.
@@ -58,101 +67,149 @@ def find_violators():
             h_name = active_violators[host]['name']
             h_ip = str(active_violators[host]['ip'])
 
-            send_to_log('{} ({}) went offline or removed from blacklist. Active for {}'.format(h_name, h_ip, active_time_calc))
+            msg = '{} ({}) is offline or removed from blacklist. Active for {}'.format(h_name, h_ip, active_time_calc)
+            send_to_log(msg)
             hosts_to_remove.append(host)
 
     for host in hosts_to_remove:
-        # print("Removing all offline hosts.")
         del active_violators[host]
 
 
-def blacklist_scanner():
-    # Check blacklisted MACs against all online hosts
+# def blacklist_scanner_arp-scan():
+#     """Scans network for blacklisted MACs and returns those active hosts."""
+#     violators = {}
+#     system_command = 'sudo arp-scan --retry={} --localnet'.format(scan_retries)
+#     p = subprocess.Popen(system_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+#
+#     for line in io.TextIOWrapper(p.stdout, encoding="utf-8"):
+#         for mac, name in blacklist_dict.items():
+#             # If arp-scan finds a blacklisted MAC and it's not a duplicate entry.
+#             if line.find(mac) != -1 and line.find("DUP") == -1:
+#                 ip = str(line.split()[0])
+#                 violators[mac] = {'name': name, 'ip': ip}
+#     return violators
+
+
+def blacklist_scanner_scapy():
+    """With scapy, scans and returns blacklisted hosts active on network."""
     violators = {}
-    system_command = 'sudo arp-scan --retry=10 --localnet'
-    p = subprocess.Popen(system_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    collection = []
+    try:
+        ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=iprange), retry=scan_retries, timeout=scan_timeout)
+    except PermissionError:
+        print('Please run with elevated permissions.')
+        sys.exit(1)
 
-    for line in io.TextIOWrapper(p.stdout, encoding="utf-8"):
+    for snd, rcv in ans:
+        result = rcv.sprintf(r"%ARP.psrc% %Ether.src%").split()
+        collection.append(result)
 
-        for mac, name in blacklist_dict.items():
-            # If arp-scan finds a blacklisted MAC and it's not a duplicate entry
-            if line.find(mac) != -1 and line.find("DUP") == -1:
-                ip = str(line.split()[0])
+    for mac, name in blacklist_dict.items():
+        for host in collection:
+            if host[1] == mac:
+                ip = host[0]
                 violators[mac] = {'name': name, 'ip': ip}
     return violators
 
 
 def find_strangers():
-    hosts_found = host_scanner()
+    """Initiates and determines if unknown hosts are active on network."""
+    strangers_found = host_scanner_scapy()
     scan_time = time.time()
 
     # Add any new strangers to dictionary.
-    for host in hosts_found:
+    for host in strangers_found:
         if host not in active_strangers.keys():
-            host_name = hosts_found[host]['name']
-            host_ip = str(hosts_found[host]['ip'])
+            host_name = strangers_found[host]['name']
+            host_ip = str(strangers_found[host]['ip'])
             active_strangers[host] = {'name': host_name, 'ip': host_ip, 'start_time': scan_time}
 
-            send_to_log('!!! STRANGER ONLINE !!! {} {} {}'.format(host_ip, host, host_name))
+            send_to_log('STRANGER ONLINE!!! {} {} {}'.format(host_ip, host, host_name))
 
-    # Now verify what strangers have gone offline.
+    # Log and remove strangers if now offline.
     hosts_to_remove = []
     for host in active_strangers.keys():
-        if host not in hosts_found:
+        if host not in strangers_found:
             diff_in_seconds = scan_time - active_strangers[host]['start_time']
             active_time_calc = str(datetime.timedelta(seconds=int(diff_in_seconds)))
             h_name = active_strangers[host]['name']
             h_ip = str(active_strangers[host]['ip'])
 
-            send_to_log('!!! STRANGER OFFLINE !!! {} {} {}. Active for {}'.format(h_ip, host, h_name, active_time_calc))
+            msg = 'STRANGER OFFLINE!!! {} {} {}. Active for {}'.format(h_ip, host, h_name, active_time_calc)
+            send_to_log(msg)
             hosts_to_remove.append(host)
 
     for host in hosts_to_remove:
         del active_strangers[host]
 
 
-def host_scanner():
-    # Collect information for all online hosts
-    online_hosts = {}
-    system_command = 'sudo arp-scan --verbose --retry=10 --localnet'
-    p = subprocess.Popen(system_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+# def host_scanner_arp-scan():
+#     """Collect information for all online hosts."""
+#     online_hosts = {}
+#     system_command = 'sudo arp-scan --verbose --retry={} --localnet'.format(scan_retries)
+#     p = subprocess.Popen(system_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+#
+#     for line in io.TextIOWrapper(p.stdout, encoding="utf-8"):
+#
+#         if len(line) > 1 and line.split()[0].count('.') == 3 and line.find("DUP") == -1:
+#             ip, mac, name, *_ = tuple(line.split())
+#
+#             if mac not in whitelist_dict.keys():
+#                 online_hosts[mac] = {'name': name, 'ip': ip}
+#
+#     return online_hosts
 
-    for line in io.TextIOWrapper(p.stdout, encoding="utf-8"):
 
-        if len(line) > 1 and line[:3] == network_prefix and line.find("DUP") == -1:
-                ip = str(line.split()[0])
-                mac = str(line.split()[1])
-                name = str(line.split()[2])
-                if mac not in whitelist_dict.keys():
-                    online_hosts[mac] = {'name': name, 'ip': ip}
+def host_scanner_scapy():
+    """With scapy, return all online hosts."""
+    online_strangers = {}
+    collection = []
+    try:
+        ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=iprange), retry=scan_retries, timeout=scan_timeout)
+    except PermissionError:
+        print('Please run with elevated permissions.')
+        sys.exit(1)
 
-    return online_hosts
+    for snd, rcv in ans:
+        result = rcv.sprintf(r"%ARP.psrc% %Ether.src%").split()
+        collection.append(result)
+
+    for host in collection:
+        ip, mac = tuple(host)
+        if mac not in whitelist_dict.keys():
+            # UNKNOWN is a remnant of returned arp-scan values, left in for future enhancements
+            online_strangers[mac] = {'name': 'UNKNOWN', 'ip': ip}
+
+    return online_strangers
 
 
 def send_to_log(entry):
+    """Writes details to log file."""
     # TODO: Implement python.logging?
     timestamp = time.strftime("%a %x %I:%M %p - ")
     log_file = "nemeses.log"
     with open(log_file, "a") as f:
         f.write('{}{}\n'.format(timestamp, entry))
-        # f.close()  # with-blocks are context managed, and close files automatically
 
 
 # Future functionality
 # Do a nmap scan on the unknown device to gather intel when you're away
-# def scan_device(ip):
-#     os.system("sudo nmap -A " + ip)
+# def nmap_scan_host(ip):
+#     system_command = 'sudo nmap -A {}'.format(ip)
+#     p = subprocess.Popen(system_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 
 # Future functionality
-# Perform a DOS on the intruding device
+# Perform a DoS on the intruding device
 # def attack_device(ip):
-#     os.system("sudo hping3 -S --flood --rand-source " + ip)
+#     system_command = 'sudo hping3 -S --flood --rand-source {}'.format(ip)
+#     p = subprocess.Popen(system_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 
+# Future functionality
 # Generate a summary for log file, showing each device active for the day and the total time online.
-def daily_totals():
-    pass
+# def daily_totals():
+#     pass
 
 
 def main():
@@ -160,9 +217,9 @@ def main():
     try:
         while True:
             update_configs()
-            find_violators()
+            find_blacklisters()
             find_strangers()
-            time.sleep(30)  # Wait for 1 minute
+            time.sleep(15)  # Wait for 1 minute
     except KeyboardInterrupt:
         print("\nUser aborted.")
     finally:
